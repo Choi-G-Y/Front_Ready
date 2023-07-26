efcaad0f605420378ad69ea0f915e339fe6c4929
CGY <lemong723@gmail.com>
2023-07-26 23:51:18 +0900

    Front_work1
    1. 메뉴 css 및 슬라이드 기능 추가 완료
    차후 세부 조정 할 수도 있음.
    
    2. 메인 포토 세션 작업 시작(출처 기입)

---
+++ Im1/js/script.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){
+	// 메뉴에 슬라이드 기능 추가 js
+    $('.navi>li').mouseover(function(){
+      $('.submenu').stop().slideDown(500);
+      $("#menu_bg").stop().slideDown(500);
+    }).mouseout(function(){
+      $('.submenu').stop().slideUp(500);
+      $("#menu_bg").stop().slideUp(500);
+    });
+
+});
